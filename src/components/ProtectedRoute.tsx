a7ca30738d9c895ded55cd6b569bdfdf1e9b163a
Marc Edison Estaca <mebestaca@gmail.com>
2026-07-25 14:29:00 -0600

    updated useauth destructuring

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,50 +1,39 @@
+import { useAuth } from "@/context/AuthContext";
 import { router } from "expo-router";
 import { ReactNode, useEffect } from "react";
-import { useAuth } from "@/context/AuthContext";
 import { ActivityIndicator, View } from "react-native";
 
 interface ProtectedRouteProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 
-export default function ProtectedRoute({
-    children
-}: ProtectedRouteProps) {
-    const {
-        user,
-        loading
-    } = useAuth();
+export default function ProtectedRoute({ children }: ProtectedRouteProps) {
+  const { user, login, logout, register, loading } = useAuth();
 
-    useEffect(()=> {
-        if (!loading && !user) {
-            router.replace("/signin");
-        }
-    }, [loading, user]);
-
-    if (loading) {
-        return (
-            <View style = {{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-                backgroundColor: "f5f7fa"
-            }}
-            >
-                <ActivityIndicator
-                    size = "large"
-                    color = "2563eb"
-                />
-            </View>
-        );
+  useEffect(() => {
+    if (!loading && !user) {
+      router.replace("/signin");
     }
+  }, [loading, user]);
 
-    if (!user) {
-        return null;
-    }
-
+  if (loading) {
     return (
-        <>
-            {children}
-        </>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "f5f7fa",
+        }}
+      >
+        <ActivityIndicator size="large" color="2563eb" />
+      </View>
     );
+  }
+
+  if (!user) {
+    return null;
+  }
+
+  return <>{children}</>;
 }
