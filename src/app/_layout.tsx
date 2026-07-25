3f2da2bde033814ca3756479f74e8675c738785d
Marc Edison Estaca <mebestaca@gmail.com>
2026-07-25 14:27:19 -0600

    fixed issue with layout not being wrapped in provider

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,52 +1,42 @@
-import { Stack } from "expo-router";
 import { AuthProvider, useAuth } from "@/context/AuthContext";
+import { Stack } from "expo-router";
 import { ActivityIndicator, View } from "react-native";
 
 export default function RootLayout() {
+  return (
+    <AuthProvider>
+      <RootLayoutNav />
+    </AuthProvider>
+  );
+}
+
+function RootLayoutNav() {
   const { loading } = useAuth();
 
   if (loading) {
     return (
-      <View style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#f5f7fa"
-      }}
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#f5f7fa",
+        }}
       >
-        <ActivityIndicator
-          size="large"
-          color="#2563eb"
-        />
+        <ActivityIndicator size="large" color="#2563eb" />
       </View>
     );
   }
-  
+
   return (
-    <AuthProvider>
-      
-      <Stack>
-        <Stack.Screen
-          name="index"
-          options={{ title: "Home" }}
-        />
-
-        <Stack.Screen
-          name="signin"
-          options={{ title: "Sign In" }}
-        />
-
-        <Stack.Screen
-          name="signup"
-          options={{ title: "Sign Up"}}
-        />
-
-        <Stack.Screen
-          name="employee"
-          options={{ title: "Employee Information" }}
-        />
-      </Stack>
-
-    </AuthProvider>
+    <Stack>
+      <Stack.Screen name="index" options={{ title: "Home" }} />
+      <Stack.Screen name="signin" options={{ title: "Sign In" }} />
+      <Stack.Screen name="signup" options={{ title: "Sign Up" }} />
+      <Stack.Screen
+        name="employee"
+        options={{ title: "Employee Information" }}
+      />
+    </Stack>
   );
 }
